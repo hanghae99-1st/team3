49edb1acb2905d63bdc6ae8d435e1afb4ee906b4
unknown <4708408@naver.com>
2022-09-22 15:13:35 +0900

    je

--- book_detail/app.py
+++ book_detail/app.py
@@ -4,6 +4,9 @@ from bs4 import BeautifulSoup
 from flask import Flask, render_template, request, jsonify
 
 from pymongo import MongoClient
+import sys
+
+
 
 client = MongoClient('mongodb+srv://test:sparta@Cluster0.zysnu4d.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -15,8 +18,7 @@ app = Flask(__name__)
 def home():
     return render_template('main.html')
 
-
-@app.route('/detail', methods=["GET"])
+@app.route("/main", methods=["GET"])
 def book_post():
     url = 'http://openapi.seoul.go.kr:8088/5172794b68717764353457746a6876/xml/bookStoreNewInfo/1/244'
 
@@ -59,33 +61,45 @@ def book_post():
         db.books.insert_one(doc)
 
     book_lists = list(db.books.find({}, {'_id': False}))
-    dict_result = []
+
+    return render_template("detail.html", data=book_lists)
+
+
+
+
+@app.route("/detail", methods=["GET"])
+def book_details():
+    book_lists = list(db.books.find({}, {'_id': False}))
+    book_result = []
+    dict_list = []
     for i in book_lists:
-        dict_list = []
         dict_list.append(i)
-        dicttolist = dict_list[0]
-        result = list(dicttolist.values())
+        for a in range(len(dict_list)):
+            a == 0
+            result = dict_list[a] # result는 딕셔너리 형태 -> value값만 뽑아옴
+            dict_value = result.values()
+            dict_result = list(dict_value)
 
-        for j in dicttolist:
+        for j in dict_result:
             dict_plus = {
-                'id': result[0],
-                'img': result[6],
-                '제목': result[1],
-                '판매여부': result[2],
-                '가격': result[3],
-                '설명': result[4],
-                '출판연도': result[5]
+                "id": dict_result[0],
+                "img": dict_result[6],
+                "제목": dict_result[1],
+                "판매여부": dict_result[2],
+                "가격": dict_result[3],
+                "설명": dict_result[4],
+                "출판연도": dict_result[5]
             }
-            dict_result.append(dict_plus)
 
-    json_data = json.dumps(dict_result)
+        book_result.append(dict_plus)
+
+    json_data = book_result
+
+    return render_template('main.html', json_data=json_data)
 
 
-    return render_template('main.html', json_data = json_data)
 
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-
